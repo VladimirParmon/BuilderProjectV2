82161be5dbb6b9193860fc683801f3eb6f1f154d
VladimirParmon <parmon_vladimir@bk.ru>
2022-11-13 12:37:53 +0300

    create a static class for defaults

---
+++ src/app/services/defaults.ts
@@ -0,0 +1,11 @@
+import {
+  FlexboxPositioningOptions,
+  FlexboxFlowOptions,
+} from '../../constants/models';
+
+export class Defaults {
+  public static defaultImageWidth = 200;
+  public static justifyContent: FlexboxPositioningOptions.CENTER;
+  public static alignItems: FlexboxPositioningOptions.CENTER;
+  public static flow: FlexboxFlowOptions.ROW;
+}
